37ece09d90b1a6befbc6c21f4af37215e5187862
Raj1778 <164516744+Raj1778@users.noreply.github.com>
2025-07-06 22:15:35 +0530

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 import { Geist, Geist_Mono, Poppins, Sora } from "next/font/google";
