3d9241e91e8496f5517c0af17a0a4261d836a4a2
Raj1778 <164516744+Raj1778@users.noreply.github.com>
2025-07-08 11:12:50 +0530

    Removed 404 pages

---
+++ app/chat/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Chat here</div>;
+};
+
+export default page;
---
+++ app/search/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Find a Friend</div>;
+};
+
+export default page;
